92227471e19591f0e13317ab622b734a2e3c3cfc
Carlo Arayon <cearayon@gmail.com>
2023-02-15 22:51:02 -0700

    Create Button Component and IButtonProps interface

--- src/components/core/Button.tsx
+++ src/components/core/Button.tsx
@@ -1,5 +1,19 @@
-import { ButtonBase } from "@mui/material";
+export interface IButtonProps {
+  text: string;
+  className: string;
+  onClick?: () => void;
+}
 
-export const Button: React.FC = () => <ButtonBase></ButtonBase>;
+export const Button: React.FC<IButtonProps> = ({
+  text,
+  className,
+  onClick,
+}) => {
+  return (
+    <button className={className} onClick={onClick}>
+      {text}
+    </button>
+  );
+};
 
 export default Button;
